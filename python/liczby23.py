785a4967f5a266ce1b50f734c9d5d3b30b28a4f8
Ola160501 <Aleksandra160501@poczta.fm>
2018-10-11 07:58:18 +0200

    petla 3

--- python/liczby23.py
+++ python/liczby23.py
@@ -18,13 +18,21 @@ def liczby2():
     return ile
     
 def liczby3():
-     Funkcja drukuje wszystkie liczby 3-cyfrowe, w których
-    nie powtarzają się cyfry.Na koniec zwraca ilość takich liczb.
-    Wykluczone liczby 111, 222, 333 itd.
+  
+    ile = 0
+    for i in range(1, 10):
+        for j in range(10):
+            for k in range(10):
+                if i != j and i != k and j !=k:
+                    print("{}{}{} ".format(i, j,k), end="")
+                    ile = ile + 1
+    
+    return ile
 
 
 def main(args):
     print("Liczb 2-cyfrowych:", liczby2())
+    print("\n\nliczb 3-cyfrowych:", liczby3())
     return 0
 
 if __name__ == '__main__':
---
+++ python/potega.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  potega.py
+#  
+
+ 
+def potega_it(a, n):
+    wynik = 1
+    
+    for i in range(n):
+        wynik = wynik * a
+        print(wynik)
+        
+    return wynik
+
+def main(args):
+    a, n = 3, 4
+    print("Potęga {} do {} wynosi {}" .format(a, n, potega_it(a, n )))
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
