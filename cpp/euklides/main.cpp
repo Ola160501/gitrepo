a54f01d7b241cd11dfcbf521453ef5b9b963d6e2
Ola160501 <Aleksandra160501@poczta.fm>
2018-10-25 07:59:24 +0200

    euklides

--- cpp/euklides/main.cpp
+++ cpp/euklides/main.cpp
@@ -3,24 +3,38 @@
 using namespace std;
 
 int nwd_klasyczny(int a, int b) {
+    int licznik = 0
     while(a != b) {
         if(a > b)
             a = a - b;
         else
             b = b - a;
-    };
+        licznik++;
+    }
+    cout << "Powtórzeń;" << licznik << end);
     return a;
 }
 
-int main()
-{
+int nwd_optymalny(int a, int b) {
+    while (a !=b0) {
+        if (a=a % b)
+            b = b * a;
+            licznik ++
+
+
+    }
+    return b;
+}
 
     int a, b;
     a = b = 0;
     cout << "podaj dwie liczby: ";
     cin >> a >> b;
     cout << "NWD(" << a << " , " << b << ") = "
-        << nwd_klasyczny(a, b) << endl;
+        << wynik << endl;
+
+    wynik = nwd_optymalny(a, b);
+        << nvd klasyczny ( a, b) << endl;
 
     return 0;
 }
