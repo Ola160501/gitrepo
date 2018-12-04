710f10cfa176ca22f960a0a31763eab27c0ea7a4
Ola160501 <Aleksandra160501@poczta.fm>
2018-12-04 12:35:51 +0100

    horner

--- cpp/horner.cpp
+++ cpp/horner.cpp
@@ -10,15 +10,21 @@
  */
 #include <iostream>
 using namespace std;
-
-void drukujw(int stopien, float tbwsp[] {
+void drukujw(int st, float tbwsp[]) {
     int i = 0;
     for(i = 0; i < st; i++) {
         cout << tbwsp[i] << "x^" << st-i << " + ";
     }
     cout << tbwsp[i] << endl;
 }
-
+float horner_it(int st, float tbwsp[], float x) {
+    // x (x (2x + 3) + 5) + 4
+    float wynik = tbwsp[0];
+    for (int i = 1; i < st+1; i++) {
+        wynik = wynik * x + tbwsp[i];
+    }
+    return wynik;
+}
 int main(int argc, char **argv)
 {
     int stopien = 0;
@@ -28,19 +34,21 @@ int main(int argc, char **argv)
     float *tbwsp;  // wskaźnik
     tbwsp = new float [stopien+1];
     for (int i=0; i <= stopien; i++) {
-        cout << "Podaj współczynnik przy potędze " << stopien-i << ": ";
+        cout << "Podaj współczynnik przy potędze "
+             << stopien-i << ": ";
         cin >> tbwsp[i];
     }
     
     cout << "Podaj argument: ";
     cin >> x;
     
-    cout << "Wartość wielomianu o postaci: "
+    cout << "Wartość wielomianu o postaci: ";
     drukujw(stopien, tbwsp);
-    
+    cout << "\ndla x = " << x << " wynosi: ";
+    cout << horner_it(stopien, tbwsp, x);
+    cout << endl;
     
     return 0;
 }
-
 
 
