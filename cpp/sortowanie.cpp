acd60cbc559cf72175b4250fb1d77c90535d80c5
Ola160501 <Aleksandra160501@poczta.fm>
2018-12-13 07:55:26 +0100

    sortowanie

---
+++ cpp/sortowanie.cpp
@@ -0,0 +1,57 @@
+s (67 sloc) 1.56 KB
+/*
+ * sortowanie.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void wypelnij_los(int tab[], int roz) {
+    srand(time(NULL));  // inicjacja generatora liczb pseudolosowych
+    for(int i = 0; i < roz; i++) {
+        tab[i] = rand() % 101;
+    }
+}
+
+void drukuj(int tab[], int roz) {
+    for(int i = 0; i < roz; i++) {
+        cout << tab[i] << " ";
+    }
+}
+
+void zamien(int &a, int &b) {
+    int tmp = a;  // zmienna pomocnicza
+    a = b;
+    b = tmp;
+}
+
+void zamien2(int tab[], int i) {
+    int tmp = tab[i];
+    tab[i] = tab[i+1];
+    tab[i+1] = tmp;
+}
+
+void sort_bubble(int tab[], int n) {
+    cout << "\nSortowanie bąbelkowe\n";
+    int licznik = 0;
+    for (int j = n - 1; j > 0 ; j--) {
+        for(int i = 0; i < j; i++) {
+            licznik++;
+            if (tab[i] > tab[i+1])
+                //zamien(tab[i], tab[i+1]);
+                zamien2(tab, i);
+        }
+}
+
+
+int main(int argc, char **argv)
+{
+	int roz = 20;
+    int tab[roz];
+    wypelnij_los(tab, roz);
+    drukuj(tab, roz);
+    zamien(5, 10)
+	return 0;
+}
+
