cad264be70d4f8c4dd41178fa9302cce6a517c8d
Ola160501 <Aleksandra160501@poczta.fm>
2018-12-19 08:45:54 +0100

    cw_petle

---
+++ cpp/cw_petle.cpp
@@ -0,0 +1,35 @@
+/*
+ * cw_petle.cpp
+
+ */
+
+
+#include <iostream>
+using namespace std;
+void cw01() {
+    int suma= 0;
+    int liczba;
+    while (suma<=75){
+    cout << "Podaj liczbę ";
+    cin >> liczba;
+        suma = suma + liczba;
+}
+    cout << "suma liczb:" << suma << endl;
+}
+     
+void cw02() {
+    int n;
+    int m;
+    for(int i=n; i<=m; i++);
+    cout << " " << i << " ";
+
+}
+void cw03() {
+    ;
+}
+int main(int argc, char **argv)
+{
+    cw01();
+	return 0;
+}
+
